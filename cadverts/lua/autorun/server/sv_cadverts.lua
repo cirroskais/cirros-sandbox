cc4a1da9ddc5c99065086a78b1ecc7bca52178f4
cirroskais <cirroskais@madhouselabs.net>
2024-09-04 13:48:04 -0400

    yeeeep

--- cadverts/lua/autorun/server/sv_cadverts.lua
+++ cadverts/lua/autorun/server/sv_cadverts.lua
@@ -1,33 +1,38 @@
 if !CLib then return error("[CAdverts] Missing required dependency CLib!") end
-include("cadverts_config.lua")
 CAdvertsTimers = CAdvertsTimers or {}
 
-for i,v in pairs(CAdvertsTimers) do
-    timer.Remove(i)
-    CAdvertsTimers[i] = nil
-end
+function Load() 
+    include("cadverts_config.lua")
+    for i,v in pairs(CAdvertsTimers) do
+        timer.Remove(i)
+        CAdvertsTimers[i] = nil
+    end
 
-if #CAdverts.Repeating then
-    for i,v in pairs(CAdverts.Repeating) do
-        timer.Create(v.name, v.every, 0, function()
-            CLib.Chat.GenericMessage(v.content, { formatColor = true })
+    if #CAdverts.Repeating > 0 then
+        for i,v in pairs(CAdverts.Repeating) do
+            timer.Create(v.name, v.every, 0, function()
+                CLib.Chat.GenericMessage(v.content, { formatColor = true })
+            end)
+
+            CAdvertsTimers[v.name] = true
+
+            print("[CAdverts] Loaded repeating advert: " .. v.name)
+        end
+    end
+
+    local curAdvert = 1
+    if #CAdverts.Rotation.adverts > 0 then
+        timer.Create("rotation", CAdverts.Rotation.every, 0, function()
+            if curAdvert > #CAdverts.Rotation.adverts then curAdvert = 1 end
+            CLib.Chat.GenericMessage(CAdverts.Rotation.adverts[curAdvert], { formatColor = true })
+            curAdvert = curAdvert + 1
         end)
 
-        CAdvertsTimers[v.name] = true
+        CAdvertsTimers.rotation = true
 
-        print("[CAdverts] Loaded repeating advert: " .. v.name)
+        print("[CAdverts] Loaded rotating advert")
     end
 end
 
-local curAdvert = 1
-if #CAdverts.Rotation and #CAdverts.Rotation.adverts then
-    timer.Create("rotation", CAdverts.Rotation.every, 0, function()
-        if curAdvert > #CAdverts.Rotation.adverts then curAdvert = 1 end
-        CLib.Chat.GenericMessage(CAdverts.Rotation.adverts[curAdvert], { formatColor = true })
-        curAdvert = curAdvert + 1
-    end)
-
-    CAdvertsTimers.rotation = true
-
-    print("[CAdverts] Loaded rotating advert")
-end
+concommand.Add("cadverts_reload", Load)
+Load()
--- cadverts/lua/cadverts_config.lua
+++ cadverts/lua/cadverts_config.lua
@@ -6,16 +6,16 @@ CAdverts = CAdverts or {}
 CAdverts.Rotation = {
     every = 10,
     adverts = {
-        "[&#ff0000]Lorem [&#00ff00]ipsum [&#ffff00]dolor",
-        "[&#0000ff]sit [&#ff00ff]amet, [&#00ffff]consectetur",
-        "[&#000000]adipiscing",
+    --     "[&#ff0000]Lorem [&#00ff00]ipsum [&#ffff00]dolor",
+    --     "[&#0000ff]sit [&#ff00ff]amet, [&#00ffff]consectetur",
+    --     "[&#000000]adipiscing",
     }
 }
 
 CAdverts.Repeating = {
-    {
-        name = "Example",
-        every = 20,
-        content = "[&#ff0000]FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK"
-    },
+    -- {
+    --     name = "Example",
+    --     every = 20,
+    --     content = "[&#ff0000]FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK"
+    -- },
 }
