4937f016f3dd3d2b2e4b828f78f15f9ef214178c
cirroskais <cirroskais@madhouselabs.net>
2024-09-06 20:07:34 -0400

    stuff

--- cadverts/lua/cadverts_config.lua
+++ cadverts/lua/cadverts_config.lua
@@ -7,9 +7,10 @@ CAdverts.Rotation = {
     every = 300,
     adverts = {
         "[&#bac2de]Join our [&#5865f2]Discord [&#bac2df]server to suggest an addon or chat!\n[&#8caaee]https://discord.gg/vGc5DtzyRm",
-        "[&#bac2de]Missing models or textures? Make sure to subscribe to the workshop collection!\n[&#8caaee]https://steamcommunity.com/sharedfiles/filedetails/?id=3307830375",
+        "[&#bac2de]Missing textures? Make sure to subscribe to the workshop collection!\n[&#8caaee]https://steamcommunity.com/sharedfiles/filedetails/?id=3307830375",
         "[&#bac2de]Tired of being killed? Run [&#a6d189]!build [&#bac2df]to enter buildmode.",
         "[&#bac2de]Done building? Run [&#e78284]!pvp [&#bac2df]to exit buildmode.",
+        "[&#bac2de]New to the server, and missing models? Run [&#a6d189]!cworkshop [&#bac2df]to download missing content!",
     }
 }
 
--- cworkshop/lua/ulx/modules/sh/cworkshop.lua
+++ cworkshop/lua/ulx/modules/sh/cworkshop.lua
@@ -1,10 +1,17 @@
-util.AddNetworkString("CWORKSHOP_OPEN_MENU")
+if SERVER then
+    util.AddNetworkString("CWORKSHOP_OPEN_MENU")
+end
 
 function ulx.cworkshop(calling_ply) 
-    net.Start("CWORKSHOP_OPEN_MENU")
-        net.WriteBool(true)
-    net.Send(calling_ply)
+    if SERVER then
+        net.Start("CWORKSHOP_OPEN_MENU")
+            net.WriteBool(true)
+        net.Send(calling_ply)
+    else
+        print("BIG BIG ERROR THIS SHOULD NEVER RUN ON THE CLIENT OOPS")
+    end
 end
 
 local cworkshop = ulx.command("Menus", "ulx cworkshop", ulx.cworkshop, "!cworkshop" )
+cworkshop:defaultAccess( ULib.ACCESS_ALL )
 cworkshop:help("Open CWorkshop's GUI to manage workshop content.")
